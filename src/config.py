08e168795155ad2dc6a9b3001f076f348fd5aefe
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-03-28 07:44:38 +0100

    Parametrizzazione

---
+++ src/config.py
@@ -0,0 +1,8 @@
+SERVICE_ACCOUNT_CREDENTIALS = 'src/sa.json'
+SERVICE_ACCOUNT_SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+BASE_FOLDER = "1mM9Xn39B_TFVWspZlT2DyQViSwBnOJ-F"
+SHEET_NAME = "Squadre"
+SPREADSHEET_NAME = 'Torneo super Bello'
+ORIGINAL_SPREADSHEET_ID = "16aBDYLq6w4nd0Bc63brYlgRdveM2tGhiImRDvh4cOS4"
+RANGE_TEAM_NAMES = "!B1:B"
+ATTRIBUTO_NOME_SQUADRA = 'Nome'
