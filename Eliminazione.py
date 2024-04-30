57dc39aa56de2092cc8cf65c7a595d3e8d1c3c46
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-04-30 14:45:14 +0200

    Eliminazione con prima fase

--- Eliminazione.py
+++ Eliminazione.py
@@ -11,15 +11,24 @@ class Eliminazione:
         # Definisco la lista delle 16 squadre
         # teams = [f"Team {i+1}" for i in range(16)]
 
+        import math
         TEAMS_COUNT = 32
-
+        GROUPS = ['A', 'B', 'C', 'D']
+        TEAMS_PER_GROUP = TEAMS_COUNT // len(GROUPS)  # Squadre per girone
+        TOTAL_ROUNDS = int(math.log2(TEAMS_COUNT))
         # Creo la struttura dati desiderata con i risultati nulli all'inizio
         self.tournament_data = dict(participant=[], stage=[], group=[], round=[], match=[], match_game=[])
         # self.tournament_data['participant'] = [{"id": i, "tournament_id": 0, "name": name.nome}
         #                                        for i, name in enumerate(teams)]
-        self.tournament_data["participant"] = [{"id": i, "tournament_id": 0, "name": name} for i, name in
-                                               enumerate([f'Team {i}' for i in range(TEAMS_COUNT)])]
-        self.tournament_data["participant"].append({"id": -1, "tournament_id": 0, "name": ""})
+        # Creazione dei partecipanti suddivisi per girone
+        self.tournament_data["participant"] = [
+            {"id": i, "tournament_id": 0,
+             "name": f'Team {(i % TEAMS_PER_GROUP) + 1} Group {GROUPS[i // TEAMS_PER_GROUP]}',
+             "group": GROUPS[i // TEAMS_PER_GROUP]}
+            for i in range(TEAMS_COUNT)]
+        # self.tournament_data["participant"] = [{"id": i, "tournament_id": 0, "name": name} for i, name in
+        #                                        enumerate([f'Team {i}' for i in range(TEAMS_COUNT)])]
+        self.tournament_data["participant"].append({"id": -1, "tournament_id": 0, "name": "", "group": -1})
 
         self.tournament_data['stage'] = [{
             "id": 0,
@@ -39,16 +48,34 @@ class Eliminazione:
         self.tournament_data["group"] = [{"id": 0, "stage_id": 0, "number": 1}]
 
         # Generazione dei round in base al numero di partecipanti.
-        import math
-        total_rounds = int(math.log2(TEAMS_COUNT))
-        for i in range(total_rounds):
+        for i in range(TOTAL_ROUNDS):
             self.tournament_data["round"].append({"id": i, "number": i + 1, "stage_id": 0, "group_id": 0})
 
         # Generazione dei match.
         match_id = 0
-        for round_id in range(total_rounds):
+        for round_id in range(TOTAL_ROUNDS):
             num_matches = TEAMS_COUNT // (2 ** (round_id + 1))
+            print("NUm:", num_matches)
             for match_number in range(num_matches):
+                print("Creo la partita ", match_number, "del round", round_id)
+                # Determinazione degli ID dei team per il primo round
+                if round_id == 0:  # Solo il primo round ha abbinamenti speciali
+                    index1 = match_number
+                    index2 = TEAMS_PER_GROUP - 1 - match_number
+                    # if match_number % 2 == 0:  # Gironi A-D e B-C
+                    group1, group2 = ('A', 'D') if match_number < TEAMS_PER_GROUP else ('B', 'C')
+                    # else:
+                    # group1, group2 = ('D', 'A') if match_number < TEAMS_PER_GROUP  else ('C', 'B')
+                    # print("p1:", [p for p in self.tournament_data["participant"] if p['group'] == group1], index1)
+                    # print("p2:", [p for p in self.tournament_data["participant"] if p['group'] == group2], index2)
+                    opponent1 = [p for p in self.tournament_data["participant"] if p['group'] == group1][
+                        index1 % TEAMS_PER_GROUP]
+                    opponent2 = [p for p in self.tournament_data["participant"] if p['group'] == group2][
+                        index2 % TEAMS_PER_GROUP]
+                else:
+                    opponent1 = {"id": -1}  # Placeholder per i round successivi
+                    opponent2 = {"id": -1}  # Placeholder per i round successivi
+
                 self.tournament_data["match"].append({
                     "id": match_id,
                     "number": match_number + 1,
@@ -57,8 +84,8 @@ class Eliminazione:
                     "round_id": round_id,
                     "child_count": 0,
                     "status": 0,
-                    "opponent1": {"id": -1},
-                    "opponent2": {"id": -1}
+                    "opponent1": opponent1,
+                    "opponent2": opponent2
                 })
                 match_id += 1
         # tournament_data = {
