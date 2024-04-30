fe70ff00110d72c4958223679027c887d1462011
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-04-30 14:17:15 +0200

    Eliminazione

--- Eliminazione.py
+++ Eliminazione.py
@@ -11,12 +11,15 @@ class Eliminazione:
         # Definisco la lista delle 16 squadre
         # teams = [f"Team {i+1}" for i in range(16)]
 
+        TEAMS_COUNT = 32
+
         # Creo la struttura dati desiderata con i risultati nulli all'inizio
         self.tournament_data = dict(participant=[], stage=[], group=[], round=[], match=[], match_game=[])
         # self.tournament_data['participant'] = [{"id": i, "tournament_id": 0, "name": name.nome}
         #                                        for i, name in enumerate(teams)]
         self.tournament_data["participant"] = [{"id": i, "tournament_id": 0, "name": name} for i, name in
-                                               enumerate([f'Team {i}' for i in range(16)])]
+                                               enumerate([f'Team {i}' for i in range(TEAMS_COUNT)])]
+        self.tournament_data["participant"].append({"id": -1, "tournament_id": 0, "name": ""})
 
         self.tournament_data['stage'] = [{
             "id": 0,
@@ -25,7 +28,7 @@ class Eliminazione:
             "type": "single_elimination",
             "number": 1,
             "settings": {
-                "size": 16,
+                "size": TEAMS_COUNT,
                 "seedOrdering": ["natural", "natural", "reverse_half_shift", "reverse"],
                 "grandFinal": "grand_final",
                 "grandFinalType": "simple",
@@ -37,14 +40,14 @@ class Eliminazione:
 
         # Generazione dei round in base al numero di partecipanti.
         import math
-        total_rounds = int(math.log2(16))
+        total_rounds = int(math.log2(TEAMS_COUNT))
         for i in range(total_rounds):
             self.tournament_data["round"].append({"id": i, "number": i + 1, "stage_id": 0, "group_id": 0})
 
         # Generazione dei match.
         match_id = 0
         for round_id in range(total_rounds):
-            num_matches = 16 // (2 ** (round_id + 1))
+            num_matches = TEAMS_COUNT // (2 ** (round_id + 1))
             for match_number in range(num_matches):
                 self.tournament_data["match"].append({
                     "id": match_id,
@@ -54,8 +57,8 @@ class Eliminazione:
                     "round_id": round_id,
                     "child_count": 0,
                     "status": 0,
-                    "opponent1": None,
-                    "opponent2": None
+                    "opponent1": {"id": -1},
+                    "opponent2": {"id": -1}
                 })
                 match_id += 1
         # tournament_data = {
